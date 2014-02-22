e25d90bafa5b2b3d520966854a0456d3d01f254d
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2014-02-22 14:44:48 +0100

    create table for actions

---
+++ app/code/community/Hackathon/FrontendMonitoring/sql/hackathon_frontendmonitoring_setup/install-1.0.0.php
@@ -0,0 +1,61 @@
+<?php
+/* @var $this Mage_Core_Model_Resource_Setup */
+$this->startSetup();
+
+$table = $this->getConnection()->newTable($this->getTable('hackathon_frontendmonitoring/user_action'));
+
+$table->addColumn(
+    'action_id',
+    Varien_Db_Ddl_Table::TYPE_INTEGER,
+    10,
+    array(
+        'primary' => true,
+    ),
+    'Action ID'
+);
+
+$table->addColumn(
+    'session_id',
+    Varien_Db_Ddl_Table::TYPE_TEXT,
+    null,
+    array(),
+    'Session id of the active user'
+);
+
+$table->addColumn(
+    'customer_id',
+    Varien_Db_Ddl_Table::TYPE_INTEGER,
+    null,
+    array(
+        'unsigned' => true,
+        'nullable' => false,
+    )
+);
+
+$table->addColumn(
+    'model',
+    Varien_Db_Ddl_Table::TYPE_TEXT,
+    255,
+    array(),
+    'Model on which the action occured'
+);
+
+$table->addColumn(
+    'action',
+    Varien_Db_Ddl_Table::TYPE_TEXT,
+    255,
+    array(),
+    'Action which occured on the model'
+);
+
+$table->addColumn(
+    'timestamp',
+    Varien_Db_Ddl_Table::TYPE_TIMESTAMP,
+    null,
+    array(),
+    'Timestamp when the action occured'
+);
+
+$ddl = $this->getConnection()->createTable($table);
+
+$this->endSetup();
