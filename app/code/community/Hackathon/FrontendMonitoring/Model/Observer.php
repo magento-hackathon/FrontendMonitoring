18986076c2610ecc7975ab30129fcdd5219c638f
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2014-02-22 15:09:53 +0100

    check if model is on the whitelist

--- app/code/community/Hackathon/FrontendMonitoring/Model/Observer.php
+++ app/code/community/Hackathon/FrontendMonitoring/Model/Observer.php
@@ -2,15 +2,20 @@
 
 class Hackathon_FrontendMonitoring_Model_Observer
 {
+    const LOG_ACTION_WHITELIST_PREFIX = 'hackathon_frontendmonitoring/log_action/';
+
     public function modelSaveAfter(Varien_Event_Observer $observer)
     {
-        $action = Mage::getModel('hackathon_frontendmonitoring/user_action');
-        $action->setSessionId(Mage::getSingleton('core/session')->getSessionId());
-        $action->setCustomerId(Mage::getSingleton('customer/session')->getCustomerId());
-        $action->setModel(get_class($observer));
-        $action->setAction('save_after');
-        $action->setTimestamp(now());
+        // only log if the model is on the whitelist
+        if (Mage::getStoreConfig(self::LOG_ACTION_WHITELIST_PREFIX . get_class($observer)) !== null) {
+            $action = Mage::getModel('hackathon_frontendmonitoring/user_action');
+            $action->setSessionId(Mage::getSingleton('core/session')->getSessionId());
+            $action->setCustomerId(Mage::getSingleton('customer/session')->getCustomerId());
+            $action->setModel(get_class($observer));
+            $action->setAction('save_after');
+            $action->setTimestamp(now());
 
-        $action->save();
+            $action->save();
+        }
     }
 }
